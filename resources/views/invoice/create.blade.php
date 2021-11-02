4c8bb4a9904c0c4dac7df70645c1b62035a861ce
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2021-11-02 15:34:39 +0200

    Finish Javascript Functionality for create page

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -72,7 +72,7 @@
                                         </td>
                                         <td>
                                             <x-form.input type="number" name="row_sub_total[]" class="row_sub_total"
-                                                         value="0.00" readonly/>
+                                                          value="0.00" readonly/>
                                         </td>
                                     </tr>
                                     </tbody>
@@ -89,7 +89,8 @@
                                         <td colspan="3"></td>
                                         <td colspan="2">sub_total</td>
                                         <td>
-                                            <x-form.input type="number" name="sub_total" class="sub_total" value="0.00" readonly/>
+                                            <x-form.input type="number" name="sub_total" class="sub_total" value="0.00"
+                                                          readonly/>
                                         </td>
                                     </tr>
                                     <tr>
@@ -112,7 +113,7 @@
                                     </tr>
                                     <tr>
                                         <td colspan="3"></td>
-                                        <td colspan="2">vat</td>
+                                        <td colspan="2">vat (5%)</td>
                                         <td>
                                             <x-form.input type="number" name="vat_value" class="vat_value" readonly/>
                                         </td>
@@ -121,14 +122,16 @@
                                         <td colspan="3"></td>
                                         <td colspan="2">shipping</td>
                                         <td>
-                                            <x-form.input type="number" name="shipping" class="shipping" step="0.01"/>
+                                            <x-form.input type="number" name="shipping" id="shipping" class="shipping"
+                                                          step="0.01"/>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td colspan="3"></td>
                                         <td colspan="2">total_due</td>
                                         <td>
-                                            <x-form.input type="number" name="total_due" class="total_due" readonly/>
+                                            <x-form.input type="number" name="total_due" id="total_due"
+                                                          class="total_due" readonly/>
                                         </td>
                                     </tr>
                                     </tfoot>
@@ -151,20 +154,52 @@
 @section('scripts')
     <script>
         $(function () {
-            function sumTotal(selector){
+            function sumTotal(selector) {
                 let sum = 0;
-                $(selector).each(function (){
+                $(selector).each(function () {
                     sum += +$(this).val();
                 })
                 return sum.toFixed(2)
             }
 
-            $('#invoice_details').on('keyup blur', '.quantity, .unit_price', function (){
+            function calcDiscount() {
+                const sub_totalVal = +$('.sub_total').val();
+                const discountType = $('.discount_type').val();
+                const discountVal = +$('.discount_value').val();
+                let netDiscountVal = 0;
+                if (discountVal !== 0) {
+                    netDiscountVal = discountType === 'percentage' ? (sub_totalVal * (discountVal / 100)) : discountVal;
+                }
+                return netDiscountVal;
+            }
+
+            function calcVat() {
+                const sub_totalVal = +$('.sub_total').val();
+                return ((sub_totalVal - calcDiscount()) * 0.05).toFixed(2);
+            }
+
+            function sumDueTotal() {
+                let sum = 0;
+                sum += +$('.sub_total').val();
+                sum -= calcDiscount();
+                sum += +$('#vat_value').val();
+                sum += +$('#shipping').val();
+                return sum;
+            }
+
+            $('#invoice_details').on('keyup blur', '.quantity, .unit_price, #discount_value, #shipping', function () {
                 const row = $(this).closest('tr');
-                const quantity = row.find('.quantity').val() || 0;
-                const unit_price = row.find('.unit_price').val() || 0;
-                row.find('.row_sub_total').val((quantity*unit_price).toFixed(2));
+                const quantity = +row.find('.quantity').val();
+                const unitPrice = +row.find('.unit_price').val();
+                row.find('.row_sub_total').val((quantity * unitPrice).toFixed(2));
                 $('#sub_total').val(sumTotal('.row_sub_total'))
+                $('#vat_value').val(calcVat());
+                $('#total_due').val(sumDueTotal());
+            })
+
+            $('#invoice_details #discount_type').on('change', function () {
+                $('#vat_value').val(calcVat());
+                $('#total_due').val(sumDueTotal());
             })
         })
     </script>
